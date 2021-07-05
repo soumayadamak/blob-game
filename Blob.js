f96dcfd9edf32e6e49365f55238094c77a79a09a
Soumaya Dammak <46013896+soumayadamak@users.noreply.github.com>
2021-07-05 10:19:33 -0400

    Update Blob.js

--- Blob.js
+++ Blob.js
@@ -1,10 +1,12 @@
 class Blob{
+    //Takes color and diameter of the blob to construct a blob
     constructor (color, diameter){
         this.color = color; 
         this.diameter = diameter; 
         this.radius = diameter/2;
         this.elt = $("<div></div").addClass("circle").css({'width':this.diameter + 'px', 'height': this.diameter + 'px', 'background-color': this.color});
     }
+    //this function takes the container as an input, adds the blob to the given container and returns the added blob
     addToGame(container){
         $(container).append(this.elt);
         return this; 
@@ -14,12 +16,14 @@ class Blob{
         this.elt.css('background-color',color);
         return this; 
     }
+    //this function takes a new diameter for the blob, changes the diameter and radius and returns the blob
     setDiameter(diameter){
         this.diameter = diameter; 
         this.radius = diameter /2 ; 
         this.setRadius(this.radius);
         return this; 
     }
+    //this function takes a new radius for the blob, changes the radius and left and top position of the blob and returns the blob
     setRadius(radius){
         this.radius = radius; 
         this.diameter = radius * 2
@@ -30,18 +34,23 @@ class Blob{
         this.elt.css({'width':this.diameter + 'px', 'height': this.diameter + 'px', 'top': topSide + 'px','left': leftSide+ 'px' });
         return this; 
     }
+    //returns the radius of the blob
     getRadius(){
         return this.radius;
     }
+    //returns the diameter of the blob
     getDiameter(){
         return this.diameter; 
     }
+    //returns the x coordinate of the center of the blob
     getX(){
         return this.x; 
     }
+    //returns the y coordinate of the center of the blob
     getY(){
         return this.y;
     }
+    //takes a new x coordinate for the blob, changes its left position and returns the blob
     setX(x){
         this.x = x; 
         var leftSide = this.x - this.radius; 
@@ -49,6 +58,7 @@ class Blob{
         this.elt.css('left', leftSide+ 'px');
         return this; 
     }
+    //takes a new y coordinate for the blob, changes its top position and returns the blob
     setY(y){
         this.y = y; 
         var topSide = this.y - this.radius; 
@@ -56,6 +66,7 @@ class Blob{
         this.elt.css('top', topSide + 'px');
         return this; 
     }
+    //takes another blob as input and returns whether the blob intersects with another blob or not
     intersects (other) {
         var dx = this.getX() - other.getX();
         var dy = this.getY() - other.getY();
